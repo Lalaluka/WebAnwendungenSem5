6b7224c2a53725c7acb3974d664251b6c1198fc5
Tobias Müller <tobias.mueller05@telekom.de>
2019-10-21 15:51:53 +0200

    shitty code 2

--- src/api/v1/entryController.js
+++ src/api/v1/entryController.js
@@ -1,12 +1,13 @@
-const Entry = require('../../schemas/listModel.js')
-
+const List = require('../../schemas/listModel.js')
 
 exports.patch = function (req,res) {
-    
+
 }
 
 exports.delete = function (req,res) {
-    
+
+    //TODO: Diesen Scheiß schreiben
+
 }
 
 exports.post = function (req,res) {
@@ -14,5 +15,14 @@ exports.post = function (req,res) {
 }
 
 exports.get = function (req,res) {
-    
+    List.findById(req.params.list_id, function (error, lists) {
+        if (error) {
+            res.json(error);
+        } else {
+            res.json({
+                message: "send all requested entries back",
+                data: lists.entries
+            });
+        }
+    });
 }
