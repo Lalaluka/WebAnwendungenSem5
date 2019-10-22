944c7b5675fbf3a57a9e3d9d2451df7cb3073cd5
Tobias Müller <tobias.mueller05@telekom.de>
2019-10-22 09:00:56 +0200

    Implemented entryController.js Push function

--- src/api/v1/entryController.js
+++ src/api/v1/entryController.js
@@ -1,20 +1,49 @@
 const List = require('../../schemas/listModel.js')
 
-exports.patch = function (req,res) {
+exports.patch = function (req, res) {
 
 }
 
-exports.delete = function (req,res) {
+exports.delete = function (req, res) {
 
     //TODO: Diesen Scheiß schreiben
 
 }
 
-exports.post = function (req,res) {
+exports.post = function (req, res) {
+    List.findById(req.params.list_id, function (error, lists) {
+        if (error) {
+            res.json(error);
+        } else {
 
+            if (!Array.isArray(req.body.entities)) {
+                req.body.entities = [req.body.entities];
+            }
+
+            req.body.entities.forEach(function (entity) {
+
+                let ent = JSON.parse(entity);
+
+                ent.creationDate = Date.now();
+                lists.entries.push(ent);
+            });
+
+            lists.save(function (error) {
+                if (error) {
+                    res.json(error);
+                } else {
+                    res.json({
+
+                        message: "Added Entities",
+                        data: lists
+                    });
+                }
+            });
+        }
+    });
 }
 
-exports.get = function (req,res) {
+exports.get = function (req, res) {
     List.findById(req.params.list_id, function (error, lists) {
         if (error) {
             res.json(error);
